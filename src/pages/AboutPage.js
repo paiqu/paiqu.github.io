9d6653cbb6e8a5a16f9bcb467183bdf2134298fa
Pai Qu <pai.qu@outlook.com>
2021-04-26 22:29:14 +1000

    push new build

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function AboutPage(props) {
   return (
     <div>
-      <h1>About PAGE!</h1>
+      <h1>Hello. It is About PAGE!</h1>
       <Link to="/">
         Back to Home Page
       </Link>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function HomePage(props) {
   return (
     <div>
-      <h1>HOME PAGE!</h1>
+      <h1>Home PAGE!</h1>
       <Link to="/about">
-        About Page
+        Go to About Page
       </Link>
     </div>
   )
