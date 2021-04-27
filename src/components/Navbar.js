902064c8c8eec63b5e9a24705a17c235cfec1844
Pai Qu <pai.qu@outlook.com>
2021-04-27 22:19:04 +1000

    add responsive design to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Link from "@material-ui/core/Link";
 import Grid from '@material-ui/core/Grid';
-
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +26,12 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: 'underline',
     textDecorationColor: theme.palette.secondary.main,
   },
+  "@media (max-width: 900px)": {
+    paddingLeft: 0,
+  },
+  drawerContainer: {
+    padding: "20px 30px",
+  }
 }));
 
 
@@ -30,73 +39,193 @@ export default function Navbar(props) {
   const classes = useStyles();
   const theme = useTheme();
 
-  return (
-    <div className={classes.root}>
-      <AppBar 
-        className={classes.appBar} 
-        color='transparent'
-      >
-        <Toolbar>
-          <Link
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  })
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+  const getDrawerChoices = () => {
+    return (
+      <div>
+        <Link
+          key='home'
+          variant='h5'
+          href='/'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+          }}
+        >
+          <MenuItem>Home</MenuItem>
+        </Link>
+        <Link
+          key='my-github'
+          variant='h5'
+          href='https://github.com/paiqu'
+          target="_blank"
+          color='textPrimary'
+          align='start'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+          }}
+        >
+          <MenuItem>My Github</MenuItem>
+        </Link>
+        <Link
+          key='about-me'
+          variant='h5'
+          href='/about'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+          }}
+        >
+          <MenuItem>About Me</MenuItem>
+        </Link>
+        <Link
+          key='login'
+          variant='h5'
+          href='/'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+          }}
+        >
+          <MenuItem>Login</MenuItem>
+        </Link>
+      </div>
+    );
+  };
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar>
+        <Link
+          className={classes.logo}
+          variant='h3'
+          href='/'
+          color='textPrimary'
+        >
+          Pai
+        </Link>
+        <div className={classes.grow} />
+        <Link
+          variant='h5'
+          href='/'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+            margin: '1rem',
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          variant='h5'
+          href='https://github.com/paiqu'
+          target="_blank"
+          color='textPrimary'
+          align='start'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+            margin: '1rem',
+          }}
+        >
+          My Github
+        </Link>
+        <Link
+          variant='h5'
+          href='/about'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+            margin: '1rem',
+          }}
+        >
+          About Me
+        </Link>
+        <Link
+          variant='h5'
+          href='/'
+          color='textPrimary'
+          style={{
+            textUnderlinePosition: 'under',
+            textDecorationColor: theme.palette.secondary.main,
+            margin: '1rem',
+          }}
+        >
+          Login
+        </Link>
+      </Toolbar>
+    );
+  };
+
+  const displayMobile = () => {
+      const handleDrawerOpen = () => 
+        setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    
+      const handleDrawerClose = () => 
+        setState((prevState) => ({ ...prevState, drawerOpen: false }));
+    
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: theme.palette.primary.contrastText,
+            'aria-label': 'menu',
+            'aria-haspopup': "true",
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: 'left',
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className={classes.drawerContainer}>{getDrawerChoices()}</div>
+        </Drawer>
+        <Link
             className={classes.logo}
             variant='h3'
             href='/'
             color='textPrimary'
           >
             Pai
-          </Link>
-          <div className={classes.grow} />
-          <Link
-            variant='h5'
-            href='/'
-            color='textPrimary'
-            style={{
-              textUnderlinePosition: 'under',
-              textDecorationColor: theme.palette.secondary.main,
-              margin: '1rem',
-            }}
-          >
-            Home
-          </Link>
-          <Link
-            variant='h5'
-            href='https://github.com/paiqu'
-            target="_blank"
-            color='textPrimary'
-            align='start'
-            style={{
-              textUnderlinePosition: 'under',
-              textDecorationColor: theme.palette.secondary.main,
-              margin: '1rem',
-            }}
-          >
-            My Github
-          </Link>
-          <Link
-            variant='h5'
-            href='/about'
-            color='textPrimary'
-            style={{
-              textUnderlinePosition: 'under',
-              textDecorationColor: theme.palette.secondary.main,
-              margin: '1rem',
-            }}
-          >
-            About Me
-          </Link>
-          <Link
-            variant='h5'
-            href='/'
-            color='textPrimary'
-            style={{
-              textUnderlinePosition: 'under',
-              textDecorationColor: theme.palette.secondary.main,
-              margin: '1rem',
-            }}
-          >
-            Login
-          </Link>
-        </Toolbar>
+        </Link>
+      </Toolbar>
+    );
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar 
+        className={classes.appBar} 
+        color='transparent'
+      >
+        {mobileView ? displayMobile() : displayDesktop()}
       </AppBar>
     </div>
   );
