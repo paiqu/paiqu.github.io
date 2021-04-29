8b9c1f223bfac38d053cd6eb0fb718537314e407
Pai Qu <pai.qu@outlook.com>
2021-04-29 12:53:07 +1000

    remove key

--- src/App.js
+++ src/App.js
@@ -26,22 +26,6 @@ const theme = createMuiTheme({
 });
 
 function App() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBFb9zlD7Sql2Li907qyEZeTIgS685JNEg",
-    authDomain: "paiqu-8777a.firebaseapp.com",
-    databaseURL: "https://paiqu-8777a-default-rtdb.firebaseio.com",
-    projectId: "paiqu-8777a",
-    storageBucket: "paiqu-8777a.appspot.com",
-    messagingSenderId: "278994291238",
-    appId: "1:278994291238:web:81233e678f157d468434e7",
-    measurementId: "G-1GJ8J5BHH8"
-  };
-
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
-
 
   return (
     <ThemeProvider theme={theme}>
